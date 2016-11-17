259da34b88e410bb42254006f29335964b43a988
Feldman Daniel <dani.feldman11@gmail.com>
2016-11-17 22:24:09 +0200

    MongoDB + PDF

--- maven-pdfproject/src/main/java/mongodb/Main.java
+++ maven-pdfproject/src/main/java/mongodb/Main.java
@@ -1,21 +1,25 @@
 package mongodb;
 
+import app.GUI;
+import common.ConnectionContainer;
+
 public class Main {
 
 	public static void main(String[] args) {
-		MongoDBJDBC db = new MongoDBJDBC("PDFdata", "localhost", 27017);
-		try {
-			db.createConnection();
-			DataManager dm = new DataManager(db.getDatabase());
-			//dm.createCollection("LearningData");
-			
-			String[] subtitles = {"Introdution", "Abtsract"};
-			PDF pdf = new PDF("home/LiuLiuCong", subtitles, 10, 10.3, "11", true);
-			dm.insertDocument("LearningData", pdf);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		} finally {
-			db.closeConnection();
-		}
+		final MongoDBJDBC db = new MongoDBJDBC("PDFdata", "localhost", 27017);
+		
+		
+		db.createConnection();
+		ConnectionContainer.dm = new DataManager(db.getDatabase());
+		Runtime.getRuntime().addShutdownHook(new Thread() 
+		{            
+			public void run() {                
+				//System.out.println("Exited");
+				db.closeConnection();
+			}        
+		});
+		
+		new GUI();
+
 	}
 }
