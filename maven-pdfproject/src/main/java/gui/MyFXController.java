597869e95e818ac1fcb47a1e654cf7fd48d30565
balaskol <balaskolaszlo95@gmail.com>
2016-12-07 20:00:27 +0200

    JavaFX: added action on buttons

--- maven-pdfproject/src/main/java/gui/MyFXController.java
+++ maven-pdfproject/src/main/java/gui/MyFXController.java
@@ -1,20 +1,34 @@
 package gui;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import app.TextProcessor;
+import common.PDFContainer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import mongodb.PDF;
+import weka.LearningDataSet;
 
 public class MyFXController {
 	
 	@FXML
 	private Button loadPdfButton;
 
+	@FXML
+	private Button loadFromDbButton;
+	
+	@FXML
+	private Button loadFromFileButton;
+	
+	@FXML
+	private Button saveTrainingSetButton;
+	
 	@FXML 
 	private Label pageNumber;
 
@@ -84,5 +98,57 @@ public class MyFXController {
 	public void loadPdf() {
 		openFile();
 	}
+	
+	@FXML
+	public void loadDataFromDB(){
+			PDF dbData[] = common.ConnectionContainer.dm.findAll(); 
+			PDFContainer.lds = new LearningDataSet();
+			PDFContainer.lds.addAllPDF(dbData, true);
+			PDFContainer.lds.write();
+	}
+	
+	@FXML
+	public void loadDataFromFile(){
+		Stage stage = (Stage) loadFromFileButton.getScene().getWindow();
+		FileChooser fileChooser = new FileChooser();
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Attribute Relational File Format", "arff");
+        fileChooser.getExtensionFilters().add(extFilter);
+		fileChooser.setTitle("Open Training Set File");
+		File selectedFile= fileChooser.showOpenDialog(stage);
+		if (selectedFile != null) {
+			PDFContainer.lds = new LearningDataSet(selectedFile.getAbsolutePath());
+			PDFContainer.lds.write();
+		}			
+	}
+	
+	public void saveFile(File file){
+		FileWriter fileWriter = null;
+        try {
+        	 fileWriter = new FileWriter(file);
+			 fileWriter.write(PDFContainer.lds.toString());
+			 fileWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}     
+	}
+	
+	@FXML
+	public void saveTrainingSet(){
+		Stage stage = (Stage) saveTrainingSetButton.getScene().getWindow();
+		FileChooser fileChooser = new FileChooser();
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Attribute Relational File Format", "arff");
+        fileChooser.getExtensionFilters().add(extFilter);
+        fileChooser.setTitle("Save training set");
+		File file = fileChooser.showSaveDialog(stage);
+		
+		
+		if(file!=null){
+			saveFile(file);
+		}
+	}
+	
+	
+	
+	
 
 }
