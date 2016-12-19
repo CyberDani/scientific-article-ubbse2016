e02be3f66c2fb7613c6d7f1dbd1b813650e4ac1b
balaskol <balaskolaszlo95@gmail.com>
2016-12-19 12:42:29 +0200

    Added back button to Learning page, save statistics to TXT

--- maven-pdfproject/src/main/java/frontend/gui/MyFXController.java
+++ maven-pdfproject/src/main/java/frontend/gui/MyFXController.java
@@ -1,26 +1,35 @@
 package frontend.gui;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import backend.model.PDF;
 import backend.weka.DataLearnerPredictor;
 import common.LearningAlgorithm;
 import common.PDFContainer;
+import common.Scientific;
+import frontend.app.Main;
 import frontend.app.TextProcessor;
-import backend.model.PDF;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class MyFXController {
+	
+	private TextProcessor tp;
 	
 	@FXML
 	private Text result;
@@ -36,6 +45,12 @@ public class MyFXController {
 	
 	@FXML
 	private Button tempTestButton;
+	
+	@FXML
+	private Button backButton;
+	
+	@FXML
+	private Button saveStat;
 	
 	@FXML 
 	private Label pageNumber;
@@ -84,7 +99,59 @@ public class MyFXController {
 		dataStructCombo.setItems(options);
 	}
 	
-	private void setLabels(TextProcessor tp){
+	@FXML
+	public void backToTraining(){
+		 Stage stage= (Stage) backButton.getScene().getWindow();
+		 FXMLLoader loader = new FXMLLoader();
+		 loader.setLocation(Main.class.getResource("../gui/ScientificArticleApp2.fxml"));
+		 AnchorPane myApp;
+		try {
+			 myApp = (AnchorPane) loader.load();
+			 Scene scene = new Scene(myApp);
+			 stage.setScene(scene);
+			 stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private String buildStatisticsString(){
+		PDF myPDF=tp.getPDF();
+		String statistics="PDF name:"+myPDF.getPath().split("\\\\")[5]+System.getProperty("line.separator")
+						+"Is scientific:"+System.getProperty("line.separator")
+						+"Page number:"+myPDF.getPagesNr()+System.getProperty("line.separator")
+						+"Average words in a row:"+myPDF.getWordsRow()+System.getProperty("line.separator")
+						+"Average row/paragraph:"+myPDF.getAvgRowInParagraph()+System.getProperty("line.separator")
+						+"Most used font size:"+myPDF.getFontSize()+System.getProperty("line.separator")
+						+"Number of images:"+myPDF.getImgNum()+System.getProperty("line.separator")
+						+"Bibliography available:"+myPDF.getBibliography();
+		
+		return statistics;
+	}
+	
+	@FXML
+	public void saveStatistics(){
+		 Stage stage= (Stage) backButton.getScene().getWindow();
+		 FileChooser fileChooser = new FileChooser();
+		 
+		 FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+		 fileChooser.getExtensionFilters().add(extFilter);
+		 File file = fileChooser.showSaveDialog(stage);
+		 
+		 if(file != null){
+			 try {
+				String data=buildStatisticsString();
+	            FileWriter fileWriter = null;
+	            fileWriter = new FileWriter(file);
+	            fileWriter.write(data);
+	            fileWriter.close();
+	        } catch (IOException ex) {
+	        	//logger here later
+	      }
+		}
+	}
+	
+	private void setLabels(){
 		PDF myPDF=tp.getPDF();
 		
 		result.setVisible(true);
@@ -188,7 +255,8 @@ public class MyFXController {
 			fileChooser.setTitle("Open File");
 			File selectedFile= fileChooser.showOpenDialog(stage);
 			if (selectedFile != null) {
-				//TextProcessor tp=new TextProcessor(selectedFile,Scientific.UNKNOWN);
+				tp=new TextProcessor(selectedFile,Scientific.UNKNOWN);
+				saveStat.setVisible(true);
 				//setLabels(tp);
 			}		
 		}else{
